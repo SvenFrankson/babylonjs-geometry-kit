bf3d581e208b6d82f620442fe28156bdc9d05b86
SvenFrankson <sven.frankson@gmail.com>
2026-07-23 12:05:33 +0200

    Create Async.ts

---
+++ src/Async.ts
@@ -0,0 +1,23 @@
+export async function Wait(frames: number = 1): Promise<void> {
+    return new Promise<void>(resolve => {
+        let check = () => {
+            if (frames <= 0) {
+                resolve();
+                return;
+            }
+            else {
+                frames--;
+                requestAnimationFrame(check);
+            }
+        }
+        check();
+    })
+}
+
+export async function NextFrame(): Promise<void> {
+    return new Promise<void>(resolve => {
+        requestAnimationFrame(() => {
+            resolve();
+        }) 
+    });
+}
